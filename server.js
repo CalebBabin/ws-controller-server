af6d1904438f96a065cc95d11465cf3f7c04f5d4
Caleb Babin <object.caleb@gmail.com>
2019-02-16 11:38:06 -0600

    adds basic message parsing/broadcasting system

--- server.js
+++ server.js
@@ -5,16 +5,65 @@ try {
     config = require(`${__dirname}/config/config-default.js`);
 }
 
+
+////////////////////////////////////////////////////////////
+//////////////////////  Parse      /////////////////////////
+//////////////////////  WebSocket  /////////////////////////
+//////////////////////  Messages   /////////////////////////
+////////////////////////////////////////////////////////////
+
+const parseMessage = (message, socket) => {
+    let data = {};
+    try {
+        data = JSON.parse(message);
+    } catch (e) {
+        console.error(e);
+        return;
+    }
+    
+    switch (data.c) { // "c" is short for "command"
+
+        case 'l': // "l" is short for "listen"
+            data.channels[data.d] = true;
+        break;
+
+        case 's': // "s" is short for "send"
+            broadcastMessage(data.c, data.d);
+        break;
+
+    }
+}
+
+const broadcastMessage = (channel, data) => {
+    
+    wss.clients.forEach(function each(ws) {
+        if (ws[channel] === true) {
+            ws.send(JSON.stringify(data));
+        }
+    });
+    
+}
+
+
+
+////////////////////////////////////////////////////////////
+//////////////////////  Init       /////////////////////////
+//////////////////////  WebSocket  /////////////////////////
+//////////////////////  Server     /////////////////////////
+////////////////////////////////////////////////////////////
+
 const WebSocket = require('ws');
  
 const wss = new WebSocket.Server(config.server);
 
 wss.on('connection', function (ws, req) {
+    ws.channels = [];
+
 	ws.on('pong', function () {
 		this.isAlive = true;
 	});
     ws.on('message', (message)=>{
-        incoming(message, ws);
+        parseMessage(message, ws);
     })
 })
 
