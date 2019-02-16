17956841e8fcb7414a86fbd4b71217c8f8e34b27
Caleb Babin <object.caleb@gmail.com>
2019-02-16 11:11:26 -0600

    adds default configuration file

---
+++ config/config-default.js
@@ -0,0 +1,5 @@
+module.exports = {
+    server: { // passed to the websocket server, see https://github.com/websockets/ws/blob/master/doc/ws.md#new-websocketserveroptions-callback
+        port: 8000
+    }
+}
